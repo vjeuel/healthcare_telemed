037b3be2745962a7d3c3f04da4b7856a87065728
vjeuel <vjeuel@gmail.com>
2021-06-09 22:48:04 -0500

    Added a form on Contact Us

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,9 +12,18 @@ function Footer() {
 		<footer>
 			<img src={logo} className='footerLogo' alt='Logo' />
 			<div className='socialMedia'>
-				<FontAwesomeIcon icon={faFacebookF} className='faBrands'></FontAwesomeIcon>
-				<FontAwesomeIcon icon={faTwitter} className='faBrands'></FontAwesomeIcon>
-				<FontAwesomeIcon icon={faLinkedinIn} className='faBrands'></FontAwesomeIcon>
+				<a href='https://www.facebook.com/profile.php?id=100069117375279' target='_blank' rel='noopener noreferrer'>
+					<FontAwesomeIcon icon={faFacebookF} className='faBrands'></FontAwesomeIcon>
+				</a>
+				<a href='https://twitter.com/HTelemed' target='_blank' rel='noopener noreferrer'>
+					<FontAwesomeIcon icon={faTwitter} className='faBrands'></FontAwesomeIcon>
+				</a>
+				<a
+					href='https://www.linkedin.com/in/healthcare-telemed-4a0396214/'
+					target='_blank'
+					rel='noopener noreferrer'>
+					<FontAwesomeIcon icon={faLinkedinIn} className='faBrands'></FontAwesomeIcon>
+				</a>
 			</div>
 			<p>
 				<FontAwesomeIcon icon={faCopyright} className='faIcon'></FontAwesomeIcon>2021 by Healthcare Telemed. All
--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -7,9 +7,53 @@ function Contact() {
 	return (
 		<div>
 			<Navbar />
-			<div className='contactContainer'>
-				<h3>Contact Form</h3>
-			</div>
+			<section className='contactContainer'>
+				<div className='contactBox'>
+					<h3>Contact Form</h3>
+					<div class='success-message'>Success! Thank you for registering</div>
+					<form class='contactForm'>
+						<div className='formFieldBox'>
+							<input
+								id='firstName'
+								className='formField'
+								type='text'
+								placeholder='First Name'
+								name='firstName'
+							/>
+							<span id='first-name-error'>Please enter a first name</span>
+						</div>
+						<div className='formFieldBox'>
+							<input id='lastName' className='formField' type='text' placeholder='Last Name' name='lastName' />
+							<span id='last-name-error'>Please enter a last name</span>
+						</div>
+						<div className='formFieldBox'>
+							<input id='phone' className='formField' type='text' placeholder='Phone number' name='phone' />
+							<span id='last-name-error'>Please enter a phone number</span>
+						</div>
+						<div className='formFieldBox'>
+							<input id='email' className='formField' type='text' placeholder='Email' name='email' />
+							<span id='last-name-error'>Please enter an email</span>
+						</div>
+						<div className='formFieldBox'>
+							<input id='message' className='formField' type='text' name='service' />
+							<span id='last-name-error'>Choose the type of service</span>
+						</div>
+						<div className='formFieldBox'>
+							<textarea
+								id='email'
+								className='formField formMessage'
+								type='textarea'
+								placeholder='Message'
+								name='message'
+							/>
+							<span id='last-name-error'>Please enter your message</span>
+						</div>
+						<button className='form-field' type='submit'>
+							Register
+						</button>
+					</form>
+				</div>
+			</section>
 			<Footer />
 		</div>
 	);
