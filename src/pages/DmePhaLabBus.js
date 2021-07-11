78b24d3d17c6ba36a09c4f5050369c9d8a59f3a8
vjeuel <vjeuel@gmail.com>
2021-07-10 19:26:19 -0500

    improved the looks of DMS and Providers page for an easier reading

--- src/pages/DmePhaLabBus.js
+++ src/pages/DmePhaLabBus.js
@@ -4,6 +4,8 @@ import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import '../pages/DmePhaLabBus.css';
 import ContactForm from '../components/ContactForm';
+import icon2 from '../img/icon_2.svg';
+import icon4 from '../img/icon_4.svg';
 
 function DmePhaLabBus() {
 	return (
@@ -16,23 +18,27 @@ function DmePhaLabBus() {
 							<h2>
 								We have solutions for a<br /> diversity of businesses​
 							</h2>
-							<p>
-								Once you’ve completed the application process, we’ll partner to complete one-time telemedicine,
-								Website training, and clinical protocol education for the telehealth opportunities that
-								interests you.
-							</p>
-							<div className='dmeLine'></div>
-							<p>
-								At Healthcare Telemed we have everything you need to grow your business. Our platform is{' '}
-								<strong style={{ color: 'var(--pb)' }}>safe, efficient, and HIPAA compliant.</strong>
-							</p>
-							<div className='dmeLine'></div>
-							<div className='button2'>
+							<div className='dmeTextContainer'>
+								<img src={icon2} className='dmeIcon' alt='' />
+								<p>
+									Once you’ve completed the application process, we’ll partner to complete one-time
+									telemedicine, Website training, and clinical protocol education for the telehealth
+									opportunities that interests you.
+								</p>
+							</div>
+							<div className='dmeTextContainer'>
+								<img src={icon4} className='dmeIcon' alt='' />
+								<p>
+									At Healthcare Telemed we have everything you need to grow your business. Our platform is{' '}
+									<strong style={{ color: 'var(--pb)' }}>safe, efficient, and HIPAA compliant.</strong>
+								</p>
+							</div>
+							<div className='dmeTextBoxWide'>
 								<NavLink to='/contact'>
 									<p className='button2'>Contact us today !!</p>
 								</NavLink>
 							</div>
-							<p>A representative will contact you to better service you.</p>
+							<p className='dmeTextBoxWide'>A representative will contact you to better service you.</p>
 						</div>
 						<div className='dmeImageBox'>
 							<img alt='' />
--- src/pages/Providers.js
+++ src/pages/Providers.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
 import './Providers.css';
 import ContactForm from '../components/ContactForm';
+import icon1 from '../img/icon_1.svg';
+import icon3 from '../img/icon_3.svg';
 
 function DmePhaLabBus() {
 	return (
@@ -12,46 +15,57 @@ function DmePhaLabBus() {
 				<section className='providers'>
 					<div className='providersBox'>
 						<div className='providersTextBox'>
-							<h2>Telemedicine Visits</h2>
-							<p className='providersTextBoxLine'>
-								Securely and efficiently see patients via our integrated HIPAA-compliant telemedicine platform.
-								Perfect  for consults and post visits. Telemedicine appointments with Healthcare Telemed, help
-								providers see more patients in less time and increase revenue at your practice.
-							</p>
-							<div className='providersLine'></div>
-							<h2>Video-conferencing</h2>
-							<p>
-								The most commonly reimbursed form of telemedicine is videoconferencing. In order to qualify as
-								“live video” across all states, real-time video and audio must be present, and the quality must
-								be similar to that of an in-person encounter.
-							</p>
-							<div className='providersLine'></div>
-							<h2>How we can help</h2>
-							<p>
-								Once you’ve completed the application process, we’ll partner to complete one-time telemedicine ,
-								Website training, and clinical protocol education for the telehealth opportunities that interest
-								you.
-							</p>
+							<div className='providersContainer'>
+								<img src={icon1} className='providersIcon' alt='' />
+								<h2>Telemedicine Visits</h2>
+								<p>
+									Securely and efficiently see patients via our integrated HIPAA-compliant telemedicine
+									platform. Perfect for consults and post visits. Telemedicine appointments with Healthcare
+									Telemed, help providers see more patients in less time and increase revenue at your practice.
+								</p>
+							</div>
+							<div className='providersContainer'>
+								<img src={icon3} className='providersIcon' alt='' />
+								<h2>Video-conferencing</h2>
+								<p>
+									The most commonly reimbursed form of telemedicine is videoconferencing. In order to qualify
+									as “live video” across all states, real-time video and audio must be present, and the quality
+									must be similar to that of an in-person encounter.
+								</p>
+							</div>
+							<div className='providersContainer providersContainerWide providersContainerHelp'>
+								<h2>How we can help</h2>
+								<div className='providersHelpBox'>
+									<p>
+										Once you’ve completed the application process, we’ll partner to complete one-time
+										telemedicine , Website training, and clinical protocol education for the telehealth
+										opportunities that interest you.
+									</p>
 
-							<p>
-								Based on your interests and experience, we'll start matching you with remote care opportunities
-								with a variety of partners — providing everything from lab requisitions and reviews, remote
-								patient monitoring, and virtual diagnosis and treatment.
-							</p>
+									<p>
+										Based on your interests and experience, we'll start matching you with remote care
+										opportunities with a variety of partners — providing everything from lab requisitions and
+										reviews, remote patient monitoring, and virtual diagnosis and treatment.
+									</p>
 
-							<p>
-								Start providing care on your terms — work anywhere, anytime, on your schedule. With the best
-								telehealth opportunities available nationwide, you’re in the driver’s seat.
-							</p>
-
-							<p>
+									<p>
+										Start providing care on your terms — work anywhere, anytime, on your schedule. With the
+										best telehealth opportunities available nationwide, you’re in the driver’s seat.
+									</p>
+								</div>
+							</div>
+							<p className='providersContainerWide'>
 								Grow your client base with our network of  businesses that will continually provide you with new
 								clientele.
 							</p>
-							<p className='providersHighlight'>
-								We provide access to the software platform for{' '}
-								<strong style={{ fontSize: '2rem' }}>FREE</strong> to Providers !!!
-							</p>
+							<div className='button2 providersContainerWide'>
+								<NavLink to='/contact'>
+									<p className='providersHighlight '>
+										We provide access to the software platform for{' '}
+										<strong style={{ fontSize: '2rem' }}>FREE</strong> to Providers !!!
+									</p>
+								</NavLink>
+							</div>
 						</div>
 						<div className='providersImageBox'>
 							<img alt='' />
